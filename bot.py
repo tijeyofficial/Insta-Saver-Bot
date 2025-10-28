de726b4807608691ce1c71084b0936584e099cf1
tijeyofficial <123543636+tijeyofficial@users.noreply.github.com>
2025-10-29 01:42:42 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,69 @@
+import telebot
+import instaloader
+import os
+
+BOT_TOKEN = "8388770582:AAGWe0_fg12fTmGQGqdQtkn-Rs1grQGgbDM"
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+loader = instaloader.Instaloader(
+    download_comments=False,
+    download_geotags=False,
+    download_pictures=False,
+    download_video_thumbnails=False,
+    save_metadata=False
+)
+
+# start bosilganda
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Assalomu alaykum. MD Digitalning Instagramdan video yuklaydigan botiga xush kelibsiz :) ")
+    bot.send_message(message.chat.id, "Menga Intagramdagi videoning havolasini yuboring ")
+    
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    # habardagi urlni olish
+    url = message.text
+
+    # url togriligini tekshrsh
+    try:
+        shortcode = url.split("/")[-2]
+    except IndexError:
+        bot.reply_to(message, "Siz yuborgan Link notog‘ri! Tekshirib qaytadan yuboring")
+        return
+
+    try: 
+        loader_message = bot.send_message(message.chat.id, "Video yuklanyapti...")
+
+        # videoni yuklash
+        post = instaloader.Post.from_shortcode(loader.context, shortcode)
+        loader.download_post(post, target=shortcode)
+
+        # yuklangan vidoeni nomini aniqlash
+        video_file = None
+        for file in os.listdir(shortcode):
+            if file.endswith(".mp4"):
+                video_file = os.path.join(shortcode, file)
+                break
+
+        # osha videoni yuborish
+        if video_file:
+            with open(video_file, "rb") as video:
+                bot.send_video(message.chat.id, video)
+                bot.delete_message(message.chat.id, loader_message.message_id)
+            
+            # va videoni ochirib tashlash
+            for f in os.listdir(shortcode):
+                os.remove(os.path.join(shortcode, f))
+            os.rmdir(shortcode)
+        else: 
+            # agar video topilmasa
+            bot.delete_message(message.chat.id, loader_message.message_id)
+            bot.reply_to(message, "Video topilmadi")
+
+    except Exception:
+        # xatolik yuzb bersa
+        bot.delete_message(message.chat.id, loader_message.message_id)
+        bot.reply_to(message, "video yuklashda xatolik yuz berdi")
+
+bot.infinity_polling()
